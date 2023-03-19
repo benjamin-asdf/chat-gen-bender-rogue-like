5593816586f44f0a1e8bc2f5ca13ab44113284a5
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2023-03-19 14:24:46 +0100

    Add repl connect

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil .
+      ((eval
+        progn
+        (make-local-variable 'cider-shadow-default-options)
+        (setq cider-shadow-default-options ":app")
+        (make-local-variable 'cider-default-cljs-repl)
+        (setq cider-default-cljs-repl 'shadow)))))
--- src/faster_than_light/memes/benders/main.cljs
+++ src/faster_than_light/memes/benders/main.cljs
@@ -1,14 +1,21 @@
 (ns faster-than-light.memes.benders.main
-  (:require [reagent.core :as r]
-            [reagent.dom :as rdom]))
+  (:require
+   [reagent.core :as r]
+   [reagent.dom :as rdom]))
 
 (def initial-state {})
 
-(defonce state (local-storage (r/atom initial-state) :game-state))
+(defonce state (r/atom initial-state))
 
+(def size 32)
+(def visible-dist 9)
+(def visible-dist-sq (js/Math.pow visible-dist 2))
+(def clear-dist 7)
+(def clear-dist-sq (js/Math.pow clear-dist 2))
 
 
 (defn game [state]
+  
 
   )
 
